8afaa573027d8506463adc682239df95c58a9641
JustinSheng <justin.wang4@icloud.com>
2024-03-27 10:53:59 +0800

    Finish the Add an Image Episode

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
 	
@@ -14,6 +15,7 @@ class ViewController: UIViewController {
 	@IBOutlet weak var verbTextField: UITextField!
 	@IBOutlet weak var numberSlider: UISlider!
 	@IBOutlet weak var numberLabel: UILabel!
+	@IBOutlet weak var storyPromptImageView: UIImageView!
 	
 	let storyPrompt = StoryPromptEntry()
 	
@@ -43,6 +45,52 @@ class ViewController: UIViewController {
 		storyPrompt.adjective = "smelly"
 		storyPrompt.verb = "burps"
 		storyPrompt.number = Int(numberSlider.value)
+		storyPromptImageView.isUserInteractionEnabled = true
+		let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+		storyPromptImageView.addGestureRecognizer(gestureRecognizer)
+	}
+	
+	@objc func changeImage() {
+//		let readWriteStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+//		if readWriteStatus == .authorized {
+			var configuration = PHPickerConfiguration()
+			configuration.filter = .images
+			configuration.selectionLimit = 1
+			let controller = PHPickerViewController(configuration: configuration)
+			controller.delegate = self
+			present(controller, animated: true)
+//		} else {
+//			// Request read-write access to the user's photo library.
+//			PHPhotoLibrary.requestAuthorization(for: .readWrite) { [weak self] status in
+//				switch status {
+//					case .notDetermined:
+//						// The user hasn't determined this app's access.
+//						print("The user hasn't determined this app's access")
+//					case .restricted:
+//						// The system restricted this app's access.
+//						print("The system restricted this app's access")
+//					case .denied:
+//						// The user explicitly denied this app's access.
+//						print("The user explicitly denied this app's access")
+//					case .authorized:
+//						// The user authorized this app to access Photos data.
+//						print("The user authorized this app to access Photos data")
+//						DispatchQueue.main.async {
+//							var configuration = PHPickerConfiguration()
+//							configuration.filter = .images
+//							configuration.selectionLimit = 1
+//							let controller = PHPickerViewController(configuration: configuration)
+//							controller.delegate = self
+//							self?.present(controller, animated: true)
+//						}
+//					case .limited:
+//						// The user authorized this app for limited Photos access.
+//						print("The user authorized this app for limited Photos access")
+//					@unknown default:
+//						fatalError()
+//				}
+//			}
+//		}
 	}
 	
 	func updateStoryPrompt() {
@@ -57,6 +105,29 @@ extension ViewController: UITextFieldDelegate {
 		textField.resignFirstResponder()
 		updateStoryPrompt()
 		return true
+	}
+}
+
+extension ViewController: PHPickerViewControllerDelegate {
+	func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+		print("hereee")
+		if !results.isEmpty {
+			print("hereee 22222")
+			let result = results.first!
+			let itemProvider = result.itemProvider
+			if itemProvider.canLoadObject(ofClass: UIImage.self) {
+				print("hereee 33333")
+				itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+					guard let image = image as? UIImage else {
+						return
+					}
+					DispatchQueue.main.async {
+						self?.storyPromptImageView.image = image
+						picker.dismiss(animated: true)
+					}
+				}
+			}
+		}
 	}
 }
 
